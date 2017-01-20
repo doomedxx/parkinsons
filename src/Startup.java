226d0cdf5dd0b560c9de944835bf0e857f300a5b
Wesley <wesley.dw@live.nl>
2017-01-20 14:33:25 +0100

    testtt

--- src/Startup.java
+++ src/Startup.java
@@ -1,20 +1,9 @@
-/**
- * Created by wesle on 20-1-2017.
- */
 public class Startup {
     public static void main(String[] args) {
-<<<<<<< HEAD
-<<<<<<< HEAD
 
         SimulatorView view = new SimulatorView(20, 5, 20);
-=======
-        Simulator simulator = new Simulator();
-        SimulatorView view = new SimulatorView(8, 2, 1);
->>>>>>> parent of 312948d... Startup
-=======
-        Simulator simulator = new Simulator();
-        SimulatorView view = new SimulatorView(8, 2, 1);
->>>>>>> parent of 312948d... Startup
         view.setVisible(true);
+
+
     }
 }
