febdf77d8002f4902cba95833778b805fda6fae4
Jille <Jillevdweerd@iCloud.com>
2017-01-20 19:17:28 +0100

    Startup change - simulatie werkt nu

--- src/Startup.java
+++ src/Startup.java
@@ -1,8 +1,8 @@
 public class Startup {
     public static void main(String[] args) {
 
-        SimulatorView view = new SimulatorView(30, 5, 20);
-        view.setVisible(true);
+        Simulator simulator = new Simulator();
+        simulator.run();
 
 
     }
