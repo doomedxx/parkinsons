a99ade7540aba69669572f31de6350e41925ec90
Wesley <wesley.dw@live.nl>
2017-01-20 14:12:54 +0100

    Revert "Startup"
    This reverts commit 312948d102f39166065d492316536823edc26f0f.
    
    # Conflicts:
    #	src/Startup.java

--- src/Startup.java
+++ src/Startup.java
@@ -3,10 +3,13 @@
  */
 public class Startup {
     public static void main(String[] args) {
+<<<<<<< HEAD
 
         SimulatorView view = new SimulatorView(20, 5, 20);
+=======
+        Simulator simulator = new Simulator();
+        SimulatorView view = new SimulatorView(8, 2, 1);
+>>>>>>> parent of 312948d... Startup
         view.setVisible(true);
-
-
     }
 }
