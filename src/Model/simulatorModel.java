1e18aa73f888f662b5f31b5bf09cf1c4f917cced
Wesley <wesley.dw@live.nl>
2017-01-26 11:10:48 +0100

    MVC First Implementation

---
+++ src/Model/simulatorModel.java
@@ -0,0 +1,24 @@
+package Model;
+
+/**
+ * Created by wesle on 26-1-2017.
+ */
+public class simulatorModel {
+
+    private View.CarParkView carParkView;
+
+
+
+
+
+
+
+
+
+    public void updateView() {
+        carParkView.updateView();
+    }
+
+
+
+}
---
+++ src/View/simulatorView.java
@@ -0,0 +1,22 @@
+package View;
+
+import javax.swing.*;
+
+/**
+ * Created by wesle on 26-1-2017.
+ */
+public class SimulatorView extends JFrame {
+    private SimulatorView.CarParkView carParkView;
+    private int numberOfFloors;
+    private int numberOfRows;
+    private int numberOfPlaces;
+    private int numberOfOpenSpots;
+    private Car[][][] cars;
+    private JButton pauseButton;
+    private JButton resumeButton;
+    private Simulator simulator;
+
+}
+
+
+
